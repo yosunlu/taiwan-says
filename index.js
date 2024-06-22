ad028d55ba3b3ae6cb84b7c796d170f1dcfc7fcf
yushanlu <yushanlu1031@gmail.com>
2024-06-22 18:42:18 +0800

    debug

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// mongoose.connect(process.env.PORT || 3000)
-//   .then(() => console.log('Connected to MongoDB!'))
-//   .catch((error) => console.error('Connection failed:', error));
+mongoose.connect(PORT)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((error) => console.error('Connection failed:', error));
 
